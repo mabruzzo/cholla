3b92a3482637a7f83d2c4963bdbd3a780e194510
Bob Caddy <r.Caddy@pitt.edu>
2024-02-26 15:19:21 -0500

    Fuse PCM reconstruction and Roe solver kernels

--- src/integrators/VL_3D_cuda.cu
+++ src/integrators/VL_3D_cuda.cu
@@ -156,14 +156,17 @@ void VL_Algorithm_3D_CUDA(Real *d_conserved, Real *d_grav_potential, int nx, int
   cuda_utilities::AutomaticLaunchParams static const roe_pcm_launch_params(
       Calculate_Roe_Fluxes_CUDA<reconstruction::Kind::pcm, 0>, n_cells);
   hipLaunchKernelGGL(HIP_KERNEL_NAME(Calculate_Roe_Fluxes_CUDA<reconstruction::Kind::pcm, 0>),
-                     roe_pcm_launch_params.get_numBlocks(), roe_pcm_launch_params.get_threadsPerBlock(), 0, 0,
-                     dev_conserved, Q_Lx, Q_Rx, F_x, nx, ny, nz, n_cells, gama, n_fields);
-  hipLaunchKernelGGL(HIP_KERNEL_NAME(Calculate_Roe_Fluxes_CUDA<reconstruction::Kind::pcm, 1>),
-                     roe_pcm_launch_params.get_numBlocks(), roe_pcm_launch_params.get_threadsPerBlock(), 0, 0,
-                     dev_conserved, Q_Ly, Q_Ry, F_y, nx, ny, nz, n_cells, gama, n_fields);
-  hipLaunchKernelGGL(HIP_KERNEL_NAME(Calculate_Roe_Fluxes_CUDA<reconstruction::Kind::pcm, 2>),
-                     roe_pcm_launch_params.get_numBlocks(), roe_pcm_launch_params.get_threadsPerBlock(), 0, 0,
-                     dev_conserved, Q_Lz, Q_Rz, F_z, nx, ny, nz, n_cells, gama, n_fields);
+                     roe_pcm_launch_params.get_numBlocks(),
+                     roe_pcm_launch_params.get_threadsPerBlock(), 0, 0, dev_conserved, Q_Lx, Q_Rx, F_x, nx, ny, nz, n_cells, gama,
+                     n_fields);
+  hipLaunchKernelGGL(HIP_KERNEL_NAME(Calculate_Roe_Fluxes_CUDA<reconstruction::Kind::pcm, 0>),
+                     roe_pcm_launch_params.get_numBlocks(),
+                     roe_pcm_launch_params.get_threadsPerBlock(), 0, 0, dev_conserved, Q_Ly, Q_Ry, F_y, nx, ny, nz, n_cells, gama,
+                     n_fields);
+  hipLaunchKernelGGL(HIP_KERNEL_NAME(Calculate_Roe_Fluxes_CUDA<reconstruction::Kind::pcm, 0>),
+                     roe_pcm_launch_params.get_numBlocks(),
+                     roe_pcm_launch_params.get_threadsPerBlock(), 0, 0, dev_conserved, Q_Lz, Q_Rz, F_z, nx, ny, nz, n_cells, gama,
+                     n_fields);
   #endif  // ROE
   #ifdef HLLC
   cuda_utilities::AutomaticLaunchParams static const hllc_pcm_launch_params(
@@ -297,16 +300,16 @@ void VL_Algorithm_3D_CUDA(Real *d_conserved, Real *d_grav_potential, int nx, int
       Calculate_Roe_Fluxes_CUDA<reconstruction::Kind::chosen, 1>, n_cells);
   hipLaunchKernelGGL(HIP_KERNEL_NAME(Calculate_Roe_Fluxes_CUDA<reconstruction::Kind::chosen, 0>),
                      roe_higher_order_launch_params.get_numBlocks(),
-                     roe_higher_order_launch_params.get_threadsPerBlock(), 0, 0, dev_conserved_half, Q_Lx, Q_Rx, F_x,
-                     nx, ny, nz, n_cells, gama, n_fields);
+                     roe_higher_order_launch_params.get_threadsPerBlock(), 0, 0, dev_conserved_half, Q_Lx, Q_Rx, F_x, nx, ny, nz, n_cells, gama,
+                     n_fields);
   hipLaunchKernelGGL(HIP_KERNEL_NAME(Calculate_Roe_Fluxes_CUDA<reconstruction::Kind::chosen, 1>),
                      roe_higher_order_launch_params.get_numBlocks(),
-                     roe_higher_order_launch_params.get_threadsPerBlock(), 0, 0, dev_conserved_half, Q_Ly, Q_Ry, F_y,
-                     nx, ny, nz, n_cells, gama, n_fields);
+                     roe_higher_order_launch_params.get_threadsPerBlock(), 0, 0, dev_conserved_half, Q_Ly, Q_Ry, F_y, nx, ny, nz, n_cells, gama,
+                     n_fields);
   hipLaunchKernelGGL(HIP_KERNEL_NAME(Calculate_Roe_Fluxes_CUDA<reconstruction::Kind::chosen, 2>),
                      roe_higher_order_launch_params.get_numBlocks(),
-                     roe_higher_order_launch_params.get_threadsPerBlock(), 0, 0, dev_conserved_half, Q_Lz, Q_Rz, F_z,
-                     nx, ny, nz, n_cells, gama, n_fields);
+                     roe_higher_order_launch_params.get_threadsPerBlock(), 0, 0, dev_conserved_half, Q_Lz, Q_Rz, F_z, nx, ny, nz, n_cells, gama,
+                     n_fields);
   #endif  // ROE
   #ifdef HLLC
   cuda_utilities::AutomaticLaunchParams static const hllc_higher_order_launch_params(
