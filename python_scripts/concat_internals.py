5553df49884027acb7d23390db8586d04bedd7df
Matthew Abruzzo <matthewabruzzo@gmail.com>
2024-07-03 11:46:39 -0400

    introduce python_scripts/concat.py

--- python_scripts/concat_internals.py
+++ python_scripts/concat_internals.py
@@ -7,6 +7,9 @@ import h5py
 import argparse
 import functools
 import pathlib
+import re
+from typing import Optional
+import warnings
 
 # ==============================================================================
 def destination_safe_open(filename: pathlib.Path) -> h5py.File:
@@ -68,25 +71,58 @@ def copy_header(source_file: h5py.File, destination_file: h5py.File) -> h5py.Fil
   return destination_file
 # ==============================================================================
 
+def _integer_sequence(s: str):
+  # converts an argument string to an integer sequence
+  # -> s can be a range specified as start:stop:step. This follows mirrors
+  #    the semantics of a python slice (at the moment, start and stop are
+  #    both required)
+  # -> s can b a comma separated list
+  # -> s can be a single value
+  m = re.match(
+    r"(?P<start>[-+]?\d+):(?P<stop>[-+]?\d+)(:(?P<step>[-+]?\d+))?",
+    s)
+  if m is not None:
+    rslts = m.groupdict()
+    step = 1
+    if rslts['step'] is not None:
+      step = int(rslts.get('step',1))
+    if step == 0:
+      raise ValueError(f"The range, {s!r}, has a stepsize of 0")
+    seq = range(int(rslts['start']), int(rslts['stop']), step)
+    if len(seq) == 0:
+      raise ValueError(f"The range, {s!r}, has 0 values")
+    return seq
+  elif re.match(r"([-+]?\d+)(,[ ]*[-+]?\d+)+", s):
+    seq = [int(elem) for elem in s.split(',')]
+    return seq
+  try:
+    return [int(s)]
+  except ValueError:
+    raise ValueError(
+      f"{s!r} is invalid. It should be a single int or a range"
+    ) from None
+
+
 # ==============================================================================
-def common_cli() -> argparse.ArgumentParser:
-  """This function provides the basis for the common CLI amongst the various concatenation scripts. It returns an
-    `argparse.ArgumentParser` object to which additional arguments can be passed before the final `.parse_args()` method
-    is used.
+def add_common_cli_args(cli: argparse.ArgumentParser):
+  """Add common command-line arguments to an argparse.ArguementParser instance
+  
+  These arguments are shared among the various concatenation scripts.
 
   Parameters
   ----------
-
-  Returns
-  -------
-  argparse.ArgumentParser
-    The common components of the CLI for the concatenation scripts
+  cli: argparse.ArgumentParser
+      Instance that arguments are added to
   """
 
   # ============================================================================
   def concat_output(raw_argument: str) -> list:
     """Function used to parse the `--concat-output` argument
     """
+    warnings.warn(
+      "The -c/--concat-output flag is now deprecated. use the --snaps "
+      "flag instead"
+    )
     # Check if the string is empty
     if len(raw_argument) < 1:
       raise ValueError('The --concat-output argument must not be of length zero.')
@@ -159,14 +195,24 @@ def common_cli() -> argparse.ArgumentParser:
     return tuple([int(i) for i in cleaned_argument.split(',')])
   # ============================================================================
 
-  # Initialize the CLI
-  cli = argparse.ArgumentParser()
-
   # Required Arguments
   cli.add_argument('-s', '--source-directory', type=pathlib.Path,  required=True, help='The path to the directory for the source HDF5 files.')
   cli.add_argument('-o', '--output-directory', type=pathlib.Path,  required=True, help='The path to the directory to write out the concatenated HDF5 files.')
-  cli.add_argument('-n', '--num-processes',    type=positive_int,  required=True, help='The number of processes that were used')
-  cli.add_argument('-c', '--concat-outputs',   type=concat_output, required=True, help='Which outputs to concatenate. Can be a single number (e.g. 8), a range (e.g. 2-9), or a list (e.g. [1,2,3]). Ranges are inclusive')
+  cli.add_argument('-n', '--num-processes',    type=positive_int,  required=True,
+    help='The number of processes that were used while running Cholla.')
+
+  grp = cli.add_mutually_exclusive_group(required=True)
+  grp.add_argument(
+    '-c', '--concat-outputs',   type=concat_output,
+    help = 'DEPRECATED (use --snaps instead) Specify outputs to concatenate. Can be a single number (e.g. 8), an inclusive range (e.g. 2-9), or a list (e.g. [1,2,3]).')
+  grp.add_argument(
+    '--snaps', type=_integer_sequence, dest = "concat_outputs",
+    metavar='(NUM | START:STOP[:STEP] | N1,N2,...)',
+    help = ('Specify output(s) to concatenate. Either a single number '
+            '(e.g. 8), a range (in python slice syntax), or a list (e.g. '
+            '1,2,3)')
+  )
+    
 
   # Optional Arguments
   cli.add_argument('--skip-fields',            type=skip_fields,   default=[],   help='List of fields to skip concatenating. Defaults to empty.')
@@ -175,7 +221,28 @@ def common_cli() -> argparse.ArgumentParser:
   cli.add_argument('--compression-opts',       type=str,           default=None, help='What compression settings to use if compressing. Defaults to None.')
   cli.add_argument('--chunking',               type=chunk_arg,     default=None, nargs='?', const=True, help='Enable chunking of the output file. Default is `False`. If set without an argument then the chunk size will be automatically chosen or a tuple can be passed to indicate the chunk size desired.')
 
+
+def common_cli() -> argparse.ArgumentParser:
+  """This function provides the basis for the common CLI amongst the various
+  concatenation scripts.
+  
+  It returns a newly constructed `argparse.ArgumentParser` object to which
+  additional arguments can be passed before the final `.parse_args()` method is
+  used.
+
+  Parameters
+  ----------
+
+  Returns
+  -------
+  argparse.ArgumentParser
+    The common components of the CLI for the concatenation scripts
+  """
+  # Initialize the CLI
+  cli = argparse.ArgumentParser()
+  add_common_cli_args(cli)
   return cli
+
 # ==============================================================================
 
 def _get_source_path(proc_id : int, source_directory : pathlib.Path,
