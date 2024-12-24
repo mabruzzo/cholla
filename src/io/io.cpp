3040241ed79f3357b0124c045a9294f2f3e5f417
Matthew Abruzzo <matthewabruzzo@gmail.com>
2024-12-24 00:29:51 -0500

    commit some changes that are necessary for running profiling with cosmology

--- src/io/io.cpp
+++ src/io/io.cpp
@@ -2272,8 +2272,10 @@ void Grid3D::Read_Grid(struct Parameters P)
 // for now assumes you will run on the same number of processors
 #ifdef MPI_CHOLLA
   #ifdef TILED_INITIAL_CONDITIONS
-  sprintf(filename, "%sics_%dMpc_%d.h5", P.indir, (int)P.tile_length / 1000,
+  char buffer[1024];
+  sprintf(buffer, "%sics_%dMpc_%d.h5", P.indir, (int)P.tile_length / 1000,
           H.nx_real);  // Everyone reads the same file
+  filename = buffer;
   #else                // TILED_INITIAL_CONDITIONS is not defined
   filename += "." + std::to_string(procID);
   #endif               // TILED_INITIAL_CONDITIONS
--- src/particles/io_particles.cpp
+++ src/particles/io_particles.cpp
@@ -35,6 +35,7 @@ void Particles3D::Load_Particles_Data(struct Parameters *P)
   // Every process reads the same file
   const std::string base_fname =
       ("ics_" + std::to_string((int)P->tile_length / 1000) + "Mpc_" + std::to_string(G.nx_local) + "_particles.h5");
+  const int nfile              = P->nfile;  // output step you want to read from
     #else
   const int nfile              = P->nfile;  // output step you want to read from
   const std::string base_fname = (std::to_string(nfile) + "_particles.h5." + std::to_string(procID));
